45bdc99423cd4697d22dc18294e286953fcd237f
Ahmaddarwiche <darwich.ahmad.92@gmail.com>
2023-01-17 16:59:26 +0100

    definition des fonctions

--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ import random as rd
 notre_score  = 50
 score_ennemi = 50
 potion = 3
-def attaque_potion_valide(string):
-    if string == "attaque":
-        return True
-    if string == "potion": 
-        return True
+
+# def attaque_potion_valide(string):
+#     if string == "attaque":
+#         return True
+#     elif string == "potion": 
+#         return True
+#     else:
+#         return False
     
     
     
@@ -14,13 +17,34 @@ def attaque_potion_valide(string):
 def on_attaque_potion(string,notre_score,score_ennemi):
     if string=="attaque":
         score_ennemi -= rd.randint(5,10)
-        return score_ennemi
-    if string=="potion":
+        return True
+    elif string=="potion":
         notre_score += rd.randint(15,50)
-        return notre_score
+        return True
+    else:
+        return False
 
-print(on_attaque_potion("attaque",notre_score,score_ennemi))
-print(on_attaque_potion("potion",notre_score,score_ennemi))
+#print(on_attaque_potion("attaque",notre_score,score_ennemi))
+#print(on_attaque_potion("potion",notre_score,score_ennemi))
+
+termin=False
+while not termin:
+    choix = input("entrer attaque ou potion : ")
+    while not on_attaque_potion(choix,notre_score,score_ennemi):
+        choix = input("SVP entre un choix valide : ")
+    
+    if score_ennemi <=0:
+        termin=True
+        print("l'ennemi est mort")
+    else:
+        notre_score -= rd.randint(5,15)
+    if notre_score <=0:
+        termin=True
+        print("On'est mort")
+        
+
+
+
 
 
 
