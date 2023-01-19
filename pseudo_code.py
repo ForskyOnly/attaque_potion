eff07dff6d317032af3778cc686744d54e6de89c
ForskyOnly <foulseumsky@gmail.com>
2023-01-19 12:36:09 +0100

    nouveau pseudo code

--- pseudo_code.py
+++ pseudo_code.py
@@ -1,17 +1,33 @@
+def on_attaque():
+        pass
+def il_attaque():
+        pass
+def on_prend_potion():
+        pass
+def compteut_potion():
+        pass
+def verifie_potion():
+        pass
 
 
-
-terminer = False
-while not terminer:
-    on_attaque_potion(choix):
-        calculer_score_ennemi()
-
-    if score_ennemi <=0:
-        terminer = True
-        print("l'ennemi à perdu")
+termine = False
+while not termine:
+    choix = input()
+    if choix == "attaque" :
+        '''calculer les scores'''
+        on_attaque()
+        il_attaque()
+    elif choix == "potion" :
+        '''calculer le nb de potions'''
+        compteut_potion()
+        verifie_potion()
+        '''recalculer les scores par''' 
+        on_attaque()
+        il_attaque()
     else:
-        ennemi_attaque()
-        if notre_score <=0:
-            terminer = True
-            print("on à perdu")
-            
+        "entre un choix valide"  
+    if "test de scores":
+        "si il y a au moins un score qui vaut <= 0"
+    termin= True
+    
+        
